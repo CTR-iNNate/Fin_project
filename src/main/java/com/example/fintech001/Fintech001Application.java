e921e61eebc64737be0f2053a64038a8d2e9d307
Ahn <youn_tip@naver.com>
2024-02-19 23:55:27 +0900

    .

---
+++ src/main/java/com/example/fintech001/Fintech001Application.java
@@ -0,0 +1,13 @@
+package com.example.fintech001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fintech001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Fintech001Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fintech001/Fintech001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fintech001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fintech001ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
